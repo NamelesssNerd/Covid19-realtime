829ab4c1b7fbc9c861ba84d725a0ac16b71f46c4
NameLesssNerd <itsmesushant20xx@gmail.com>
2022-12-15 00:09:43 +0545

    Initial commit

---
+++ src/components/Country.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './country.scss';
+
+const Country = () => {
+    return (
+        <div className="container">
+            <div className="left"></div>
+            <div className="right"></div>
+        </div>
+    )
+}
+
+export default Country
